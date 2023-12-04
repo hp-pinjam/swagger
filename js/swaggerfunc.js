4c46d0a27d81fe8e06a4f2b4ac6f2f71d669bf0f
FarhanRiziq01 <farhanriziq01@gmail.com>
2023-12-04 13:05:24 +0700

    js

--- js/swaggerfunc.js
+++ js/swaggerfunc.js
@@ -3,18 +3,32 @@ import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-
 export let URLData = "https://raw.githubusercontent.com/hp-pinjam/swagger/main/yaml/openapi.yaml";
 
 export const UIData = SwaggerUIBundle({
-    url: URLData, //Location of Open API spec in the repo
+    url: URLData,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
         SwaggerUIBundle.presets.apis,
         SwaggerUIBundle.SwaggerUIStandalonePreset
     ],
+    requestInterceptor: (request) => {
+        // Tambahkan logika untuk menyertakan token ke setiap permintaan
+        // Contoh: request.headers['Login'] = 'your-api-key';
+
+        // Ambil token API key dari tempat penyimpanan atau tempat lain yang sesuai
+        const apiKey = localStorage.getItem('api_key');
+
+        // Sisipkan token API key ke header permintaan jika tersedia
+        if (apiKey) {
+            request.headers['Login'] = apiKey;
+        }
+
+        return request;
+    },
     plugins: [
         SwaggerUIBundle.plugins.DownloadUrl
     ],
 });
 
 export function setSwagger() {
-    return UIData
+    return UIData;
 }
